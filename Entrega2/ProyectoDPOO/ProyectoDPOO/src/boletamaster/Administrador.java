3d1a376cfe0eddd966f6c23c11d887dc1f2aca59
f.saavedra1 <f.saavedra1@uniandes.edu.co>
2025-10-21 13:50:27 -0500

    cambios de admin

--- Entrega2/ProyectoDPOO/ProyectoDPOO/src/boletamaster/Administrador.java
+++ Entrega2/ProyectoDPOO/ProyectoDPOO/src/boletamaster/Administrador.java
@@ -1,10 +1,27 @@
 package boletamaster;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Administrador extends Usuario{
 	
+	
+	public  HashMap<String, Evento> mapa_eventos;
+	public  HashMap<String, Venue> mapa_venues;
 	
 	public Administrador(String login, String password) {
 		super(login,password);
 	}
+	
+	public void crearVenue(String id, String nombre_evento, String nombre_venue, String ubicacion, int capacidad_maxima, String restricciones_de_uso) {
+		Evento evento_encontrado = mapa_eventos.get(nombre_evento);
+		Venue venue_creado = new Venue(id, nombre_venue, ubicacion, capacidad_maxima,restricciones_de_uso, true);
+		
+		mapa_venues.put(restricciones_de_uso, venue_creado);
+		
+		evento_encontrado.getVenues().add(venue_creado);
+	}
+	
+	
 }
 
--- Entrega2/ProyectoDPOO/ProyectoDPOO/src/boletamaster/Usuario.java
+++ Entrega2/ProyectoDPOO/ProyectoDPOO/src/boletamaster/Usuario.java
@@ -1,28 +1,25 @@
 package boletamaster;
 
 public class Usuario {
-	
-	private String login;
-	private String password;
-	
-	public Usuario(String login, String password) {
-		this.login = login;
-		this.password = password;
-	}
-	
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-	
+			private String login;
+			private String password;
+			
+			public Usuario(String login, String password) {
+				this.login = login;
+				this.password = password;
+			}
+			
+			public String getLogin() {
+				return login;
+			}
+			public void setLogin(String login) {
+				this.login = login;
+			}
+			public String getPassword() {
+				return password;
+			}
+			public void setPassword(String password) {
+				this.password = password;
+			}
+
 }
